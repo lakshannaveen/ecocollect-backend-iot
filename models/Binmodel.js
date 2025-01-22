97ec072ecbad25e69cca2a43896cc6d47369371a
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-01-22 08:27:04 +0530

    schema added temprature

--- models/Binmodel.js
+++ models/Binmodel.js
@@ -9,6 +9,7 @@ const BinSchema = new mongoose.Schema({
   fullnessPercentage: { type: Number, default: 0, min: 0, max: 100 },
   isBinFull: { type: Boolean, default: false },
   isCollected: { type: Boolean, default: false },
+  temperature: { type: Number, required: true }, 
 }, { timestamps: true });
 
 module.exports = mongoose.model('Bin', BinSchema);
